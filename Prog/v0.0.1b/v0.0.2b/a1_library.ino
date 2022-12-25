f0e9d2de2302f58c017145570782f0d22b016b83
KONKORD R-n-D <65111131+KONKORD-R-N-D@users.noreply.github.com>
2022-12-25 08:09:00 +0300

    Delete Prog/v0.0.1b directory

--- Prog/v0.0.1b/v0.0.2b/a1_library.ino
+++
@@ -1,4 +0,0 @@
-//Подключение библиотек
-#include "DHT.h"
-#include "LiquidCrystal_I2C.h"
-#include "Wire.h"
--- Prog/v0.0.1b/v0.0.2b/a4_initialization.ino
+++
@@ -1,3 +0,0 @@
-LiquidCrystal_I2C lcd(0x27, 20, 4);
-DHT dht_22(DHT_22Pin, DHT_22);
-DHT dht_11(DHT_11Pin, DHT_11);
